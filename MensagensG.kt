daa84fbbe69c5ce0e96101a413427dbc7ddad80e
MatheusHaliski <matheushaliski@gmail.com>
2025-09-12 13:24:51 +0000

    Upload New File

---
+++ MensagensG.kt
@@ -0,0 +1,21 @@
+package br.pucpr.authserver.mensagens
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "mensagens")
+open class MensagensG(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    open var id: Long? = null,
+
+    @Column(nullable = false)
+    open var emailUser: String = "",
+
+    @Column(nullable = false)
+    open var emailDestinatario: String = "",
+
+    @Column(nullable = false)
+    open var messageContent: String = ""
+) {
+    constructor() : this(null, "", "", "") // construtor no-arg obrigatório para o Hibernate
+}
