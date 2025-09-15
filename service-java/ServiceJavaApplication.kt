7fe08404988ea1db7d127c546b4d6b9daaaa9873
MatheusHaliski <matheushaliski@gmail.com>
2025-09-15 12:47:09 +0000

    Upload New File

---
+++ service-java/ServiceJavaApplication.kt
@@ -0,0 +1,11 @@
+package cloudcomputing.service_java
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceJavaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceJavaApplication>(*args)
+}
