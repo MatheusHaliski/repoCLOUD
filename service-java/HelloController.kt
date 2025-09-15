4500d820dccd5fa65b67b66487284f862453f9f1
MatheusHaliski <matheushaliski@gmail.com>
2025-09-15 13:02:02 +0000

    Update 2 files
    - /HelloController.kt
    - /service-java/HelloController.kt

--- service-java/HelloController.kt
+++ service-java/HelloController.kt
@@ -1,15 +1,26 @@
-package cloudcomputing.service_java
+package br.pucpr.authserver.mensagens
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/java")
+@RequestMapping("/api")
 class HelloController {
 
+    // GET simples
     @GetMapping("/hello")
     fun hello(): String {
-        return "Hello from service-java!"
+        return "Servidor rodando com sucesso 🚀"
+    }
+
+    // POST com JSON
+    @PostMapping("/echo")
+    fun echo(@RequestBody body: Map<String, Any>): Map<String, Any> {
+        return mapOf("recebido" to body)
+    }
+
+    // GET com parâmetro
+    @GetMapping("/soma")
+    fun soma(@RequestParam a: Int, @RequestParam b: Int): Map<String, Int> {
+        return mapOf("resultado" to (a + b))
     }
 }
